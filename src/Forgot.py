f15a84b7c58c9f77fc7ec8122e2114ef59208113
Drew Boston <31427655+dboston1@users.noreply.github.com>
2019-02-20 23:02:48 -0700

    Update Forgot.py

--- src/Forgot.py
+++ src/Forgot.py
@@ -5,37 +5,40 @@ class Forgot:
 
     def __init__(self):
         #TODO: fill with garbage values
-        self.password = "pinkpoodle"
-        self.email = "test.app10097@gmail.com"
-    
-        with smtplib.SMTP("smtp.gmail.com") as self.server:
-            self.server.starttls()
-            self.server.login(self.email, self.password)
+        pass
+        
+        #NOTE: this doesn't work anymore, google changed to an API...
+        
+        #with smtplib.SMTP("smtp.gmail.com") as self.server:
+        #    self.server.starttls()
+        #    self.server.login(self.email, self.password)
             
 
     
     def genUserMessage(self, app, email):
-        username = ""
-        for l in app._loginData._loginInfo.values():
-            print(str(l))
-            temp_email = l[1]   #l has format [password, email]
-            if temp_email == email:
-                username = l[0]
-                break
-        if username is not "":
-            message = "\nSubject: Forgotten Username\n\nHello,\nYour username is: " + str(username) + "\n\nThank you!\n  -DB"
-            return message
-        else:
-            print("error")
+        pass
+        #username = ""
+        #for l in app._loginData._loginInfo.values():
+        #    print(str(l))
+        #    temp_email = l[1]   #l has format [password, email]
+        #    if temp_email == email:
+        #        username = l[0]
+        #        break
+        #if username is not "":
+        #    message = "\nSubject: Forgotten Username\n\nHello,\nYour username is: " + str(username) + "\n\nThank you!\n  -DB"
+        #    return message
+        #else:
+        #    print("error")
             #TODO: no account associated with this email address
-            pass 
+        #    pass 
             
             
         #send email with username 
     def forgotUsername(self, app, email):
-        message = str(self.genUserMessage(app, email))
-        print("message: " + str(message))
-        self.server.sendmail(self.email, email, message)
+        pass
+        #message = str(self.genUserMessage(app, email))
+        #print("message: " + str(message))
+        #self.server.sendmail(self.email, email, message)
         #TODO: display message notifying email has been sent
         
         
